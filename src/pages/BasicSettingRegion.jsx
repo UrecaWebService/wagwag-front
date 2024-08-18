beb9002e2951985c0357e16464c47f8537fb7766
fishman <cho.y.j1997@gmail.com>
2024-08-18 23:57:12 +0900

    type : [#feature]구 군 시 선택지 초안 스타일 수정

--- src/pages/BasicSettingRegion.jsx
+++ src/pages/BasicSettingRegion.jsx
@@ -16,49 +16,66 @@ const BasicSettingRegion = () => {
             <div className="flex flex-col items-center justify-center min-h-screen text-white">
                 {/*title*/}
                 <div className="mb-6 text-center">
-                    <h1 className="text-xl font-bold">waggle 님의 지역을 설정해 주세요</h1>
-                    <p className="text-sm text-green-400">* 내 지역을 기반으로 와글을 볼 수 있어요</p>
+                    <h1 className="text-2xl font-bold"><span className="italic">waggle</span> 님의 지역을 설정해 주세요</h1>
+                    <p className="text-sm text-gray-500"><span className="text-sm text-green-400">* 내 지역</span>을 기반으로 와글을 볼 수 있어요</p>
                 </div>
 
                 <div
-                    className="flex justify-between p-6 rounded-lg w-[80%] max-w-4xl">
-                    <div className="w-1/3 pr-4">
-                        <div className="text-gray-400 mb-2">시·도</div>
-                        <div className="bg-transparent border-b border-gray-500 text-white py-2">서울</div>
+                    className="flex justify-between p-2 rounded-lg w-[100%] max-w-7xl h-80">
+                    <div className="flex-[0.4] pr-4 border-r border-gray-500">
+                        <div className="text-gray-400 mb-2 text-xl">시·도</div>
+                        <div className="mt-14 bg-transparent text-white py-2 text-2xl hover:text-green-500">서울</div>
                     </div>
+                    <div className="flex-8 w-2/5 pr-3">
+                        <div className="pl-2 text-gray-400 mb-12 font-bold text-xl">구 · 군 · 시</div>
+                        <div className="flex justify-center  ">
+                            <div className="flex flex-wrap gap-2">
+                                <button
+                                    className="mx-3 mb-2 px-9 py-4 rounded-full border-2 border-transparent hover:text-green-500 focus:text-green-500 active:text-green-500 focus:border-green-500 active:border-green-500 font-semibold text-white text-xl transition-all duration-100">
+                                    강남구
+                                </button>
 
-                    <div className="w-1/3 pr-4">
-                        <div className="text-gray-400 mb-2">구·군·시</div>
-                        <div className="flex flex-wrap gap-2">
-                            <button
-                                className="px-4 py-2 rounded-full border border-gray-500 hover:text-green-500 bg-black/50 text-white">강남구
-                            </button>
-                            <button
-                                className="px-4 py-2 rounded-full border border-gray-500 hover:text-green-500 bg-black/50 text-white">강북구
-                            </button>
-                            <button
-                                className="px-4 py-2 rounded-full border border-gray-500 hover:text-green-500 bg-black/50 text-white">강동구
-                            </button>
-                            <button
-                                className="px-4 py-2 rounded-full border border-gray-500 hover:text-green-500 bg-black/50 text-white">광진구
-                            </button>
-                            <button
-                                className="px-4 py-2 rounded-full border border-gray-500 hover:text-green-500 bg-black/50 text-white">금천구
-                            </button>
-                            <button
-                                className="px-4 py-2 rounded-full border border-gray-500 hover:text-green-500 bg-black/50 text-white">구로구
-                            </button>
-                            <button
-                                className="px-4 py-2 rounded-full border border-gray-500 hover:text-green-500 bg-black/50 text-white">강서구
-                            </button>
+
+                                <button
+                                    className="mx-3 mb-2 px-9 py-4 rounded-full border-2 border-transparent hover:text-green-500 focus:text-green-500 active:text-green-500 focus:border-green-500 active:border-green-500 font-semibold text-white text-xl transition-all duration-100">
+                                    강북구
+                                </button>
+
+                                <button
+                                    className="mx-3 mb-2 px-9 py-4 rounded-full border-2 border-transparent hover:text-green-500 focus:text-green-500 active:text-green-500 focus:border-green-500 active:border-green-500 font-semibold text-white text-xl transition-all duration-100">
+                                    강동구
+                                </button>
+
+                                <button
+                                    className="mx-3 mb-2 px-9 py-4 rounded-full border-2 border-transparent hover:text-green-500 focus:text-green-500 active:text-green-500 focus:border-green-500 active:border-green-500 font-semibold text-white text-xl transition-all duration-100">
+                                    광진구
+                                </button>
+
+                                <button
+                                    className="mx-3 mb-2 px-9 py-4 rounded-full border-2 border-transparent hover:text-green-500 focus:text-green-500 active:text-green-500 focus:border-green-500 active:border-green-500 font-semibold text-white text-xl transition-all duration-100">
+                                    금천구
+                                </button>
+
+                                <button
+                                    className="mx-3 mb-2 px-9 py-4 rounded-full border-2 border-transparent hover:text-green-500 focus:text-green-500 active:text-green-500 focus:border-green-500 active:border-green-500 font-semibold text-white text-xl transition-all duration-100">
+                                    구로구
+                                </button>
+
+                                <button
+                                    className="mx-3 mb-2 px-9 py-4 rounded-full border-2 border-transparent hover:text-green-500 focus:text-green-500 active:text-green-500 focus:border-green-500 active:border-green-500 font-semibold text-white text-xl transition-all duration-100">
+                                    강서구
+                                </button>
+                            </div>
+
+
                         </div>
                     </div>
 
-                    <div className="w-1/3">
-                        <div className="text-gray-400 mb-2">동·읍·면</div>
+                    <div className="flex-8 w-1/3">
+                        <div className="pl-2 text-gray-400 mb-12 font-bold text-xl">동 · 읍 · 면</div>
                         <div className="flex flex-wrap gap-2">
                             <button
-                                className="px-4 py-2 rounded-lg border border-gray-500 bg-black/50 text-white">개포동
+                                className="px-4 py-2 rounded-lg border border-gray-500 bg-black/50  hover:text-green-500 focus:text-green-500 active:text-green-500 text-white">개포동
                             </button>
                             <button
                                 className="px-4 py-2 rounded-lg border border-gray-500 bg-black/50 text-white">논현동
