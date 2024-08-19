fcdbeda637a9182f86911e1714829120d6f9dbf0
jueunseuk <junsu120202@gmail.com>
2024-08-19 09:57:01 +0900

    type : [#refactor] 카테고리 설정 페이지 파라미터화 작업

--- src/pages/BasicSettingCategory.jsx
+++ src/pages/BasicSettingCategory.jsx
@@ -2,7 +2,16 @@ import React, { useState } from 'react';
 import settingBg from '../assets/settingBg.png';
 import logoImage from '../assets/wagwagLogo.png';
 
-const BasicSettingCategory = () => {
+const categories = [
+    '뷰티', '운동', '노래',
+    '게임', '산책', '음식',
+    '일상생활', '타 지역 인기 와글'
+];
+
+const BUTTON_SELECTED_CLASS = 'text-secondary border-secondary';
+const BUTTON_UNSELECTED_CLASS = 'text-gray-500 border-gray-500';
+
+const BasicSettingCategory = ({ username = 'waggle' }) => {
     const [selectedButtons, setSelectedButtons] = useState([]);
 
     const handleButtonClick = (label) => {
@@ -31,67 +40,26 @@ const BasicSettingCategory = () => {
 
             {/* 가이드 텍스트 */}
             <div className="text-white text-center">
-                <h1 className="text-2xl font-bold"><span className='text-secondary'>waggle</span> 님이 관심있는 주제를 알려주세요</h1>
-                <p className="text-sm mt-2"><span className='text-secondary'>*</span> 내 취향에 맞는 와글을 더 편리하게 볼 수 있어요</p>
+                <h1 className="text-2xl font-bold">
+                    <span className='text-secondary'>{username}</span> 님이 관심있는 주제를 알려주세요
+                </h1>
+                <p className="text-sm mt-2">
+                    <span className='text-secondary'>*</span> 내 취향에 맞는 와글을 더 편리하게 볼 수 있어요
+                </p>
             </div>
 
             {/* 카테고리 선택 영역 */}
-            <div className="flex flex-col items-center mt-10 space-y-4">
-                <div className="flex space-x-4">
-                    <button
-                        className={`border rounded-full px-10 py-3 ${isSelected('Beautiful') ? 'text-secondary border-secondary' : 'text-gray-500 border-gray-500'}`}
-                        onClick={() => handleButtonClick('Beautiful')}
-                    >
-                        뷰티
-                    </button>
-                    <button
-                        className={`border rounded-full px-10 py-3 ${isSelected('Health') ? 'text-secondary border-secondary' : 'text-gray-500 border-gray-500'}`}
-                        onClick={() => handleButtonClick('Health')}
-                    >
-                        운동
-                    </button>
-                    <button
-                        className={`border rounded-full px-10 py-3 ${isSelected('Song') ? 'text-secondary border-secondary' : 'text-gray-500 border-gray-500'}`}
-                        onClick={() => handleButtonClick('Song')}
-                    >
-                        노래
-                    </button>
-                </div>
-
-                <div className="flex space-x-4">
-                    <button
-                        className={`border rounded-full px-10 py-3 ${isSelected('Game') ? 'text-secondary border-secondary' : 'text-gray-500 border-gray-500'}`}
-                        onClick={() => handleButtonClick('Game')}
-                    >
-                        게임
-                    </button>
-                    <button
-                        className={`border rounded-full px-10 py-3 ${isSelected('Walk') ? 'text-secondary border-secondary' : 'text-gray-500 border-gray-500'}`}
-                        onClick={() => handleButtonClick('Walk')}
-                    >
-                        산책
-                    </button>
-                    <button
-                        className={`border rounded-full px-10 py-3 ${isSelected('Food') ? 'text-secondary border-secondary' : 'text-gray-500 border-gray-500'}`}
-                        onClick={() => handleButtonClick('Food')}
-                    >
-                        음식
-                    </button>
-                </div>
-
-                <div className="flex space-x-4">
-                    <button
-                        className={`border rounded-full px-10 py-3 ${isSelected('Daily life') ? 'text-secondary border-secondary' : 'text-gray-500 border-gray-500'}`}
-                        onClick={() => handleButtonClick('Daily life')}
-                    >
-                        일상생활
-                    </button>
-                    <button
-                        className={`border rounded-full px-10 py-3 ${isSelected('Popular in other regions') ? 'text-secondary border-secondary' : 'text-gray-500 border-gray-500'}`}
-                        onClick={() => handleButtonClick('Popular in other regions')}
-                    >
-                        타 지역 인기 와글
-                    </button>
+            <div className="w-1/3 mx-auto mt-10 max-h-[calc(100vh-400px)] overflow-y-auto">
+                <div className="flex flex-wrap justify-center mt-10 gap-4">
+                    {categories.map((category, index) => (
+                        <button
+                            key={index}
+                            className={`border rounded-full px-10 py-3 ${isSelected(category) ? BUTTON_SELECTED_CLASS : BUTTON_UNSELECTED_CLASS}`}
+                            onClick={() => handleButtonClick(category)}
+                        >
+                            {category}
+                        </button>
+                    ))}
                 </div>
             </div>
 
